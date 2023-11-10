f919e6e9e2e99f2789a495e050e13ee0cb0c38b2
youssouf <y.aliahmed99@gmail.com>
2023-11-10 17:42:32 +0100

    genetic algorithm need to pull

--- genetic algorithm/genetic.py
+++ genetic algorithm/genetic.py
@@ -1,10 +1,11 @@
 import numpy as np  # Importing the numpy library as 'np' for efficient numerical operations.
 import random  # Importing the 'random' module to generate random numbers.
-import time
-import matplotlib.pyplot as plt
+import time  # Importing the 'time' module to measure execution time.
+import matplotlib.pyplot as plt  # Importing the 'matplotlib.pyplot' module for data visualization.
 
-#  Génétic Algorithms
+# Genetic Algorithms
 def max_segment_2d_genetic(matrix, population_size=100, num_generations=100):
+    # Function to generate a random program (candidate solution)
     def generate_program(matrix):
         rows, cols = matrix.shape
         i1 = random.randint(0, rows - 1)
@@ -13,11 +14,13 @@ def max_segment_2d_genetic(matrix, population_size=100, num_generations=100):
         j2 = random.randint(j1, cols - 1)
         return i1, i2, j1, j2
 
+    # Function to evaluate the fitness of a program
     def fitness(program, matrix):
         i1, i2, j1, j2 = program
         submatrix = matrix[i1:i2 + 1, j1:j2 + 1]
         return np.sum(submatrix)
 
+    # Function to recombine two programs
     def recombine(program1, program2):
         i1 = (program1[0] + program2[0]) // 2
         i2 = (program1[1] + program2[1]) // 2
@@ -25,6 +28,7 @@ def max_segment_2d_genetic(matrix, population_size=100, num_generations=100):
         j2 = (program1[3] + program2[3]) // 2
         return i1, i2, j1, j2
 
+    # Function to mutate a program
     def mutate(program):
         i1, i2, j1, j2 = program
         if random.random() < 0.2:
@@ -37,8 +41,10 @@ def max_segment_2d_genetic(matrix, population_size=100, num_generations=100):
             j2 = random.randint(j1, j2)
         return i1, i2, j1, j2
 
+    # Initialization of the population with random programs
     population = [generate_program(matrix) for _ in range(population_size)]
 
+    # Main loop for genetic algorithm
     for generation in range(num_generations):
         population = sorted(population, key=lambda program: -fitness(program, matrix))
 
@@ -53,9 +59,10 @@ def max_segment_2d_genetic(matrix, population_size=100, num_generations=100):
 
         population = new_population
 
+    # Return the program with the maximum fitness (maximum sum of submatrix)
     return max(population, key=lambda program: fitness(program, matrix))
 
-# Fonction pour générer une solution initiale (sous-matrice aléatoire)
+# Function to generate an initial solution (random submatrix)
 def generate_solution(matrix):
     rows, cols = matrix.shape
     i1 = random.randint(0, rows - 1)
@@ -64,48 +71,136 @@ def generate_solution(matrix):
     j2 = random.randint(j1, cols - 1)
     return i1, i2, j1, j2
 
-# Fonction pour évaluer une solution (somme des éléments de la sous-matrice)
+# Function to evaluate a solution (sum of elements in the submatrix)
 def evaluate_solution(solution, matrix):
     i1, i2, j1, j2 = solution
     submatrix = matrix[i1:i2 + 1, j1:j2 + 1]
     return np.sum(submatrix)
 
-
-import time
-import matplotlib.pyplot as plt
-
-def plot_algorithm_comparison(algorithm_names, max_sums, algorithm_name):
-    plt.figure(figsize=(8, 6))
-    plt.scatter(algorithm_names, max_sums, color='red', marker='o')
-    plt.xlabel('Algorithms')
-    plt.ylabel('Maximum Segment Sum')
-    plt.title(f'Maximum Segment Sum Comparison - {algorithm_name}')
-    plt.grid(True)  # Ajout d'une grille
-    plt.show()
-
+# Main function
 def main():
-    matrix = np.array([[1, 2, -1, -4, -20],
-                      [-8, -3, 4, 2, 1],
-                      [3, 8, 10, 1, 3],
-                      [-4, -1, 1, 7, -6]])
+  
+    # Define the list of test cases
+    tests = [
+        {
+            "matrix": [
+                [1, 2, -1, -4, -20],
+                [-8, -3, 4, 2, 1],
+                [3, 8, 10, 1, 3],
+                [-4, -1, 1, 7, -6],
+            ],
+        },
+        {
+            "matrix": [
+                [1, 2, -1, -4, -20],
+                [-8, -3, 4, 2, 1],
+                [3, 8, 10, 1, 3],
+                [-4, -1, 1, 7, -6],
+            ],
+        },
+        {
+            "matrix": [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+        },
+        {
+            "matrix": [[-1, -2, -3], [-4, -5, -6], [-7, -8, -9]],
+        },
+        {
+            "matrix": [[1, -2, 3], [4, -5, 6], [7, -8, 9]],
+        },
+        {
+            "matrix": [[1, 2], [3, 4]],
+        },
+        {
+            "matrix": [
+                [2, -1, 4, -6, 2],
+                [-3, 2, -1, 4, -3],
+                [1, -5, 2, -1, 5],
+                [4, -2, 3, 7, -2],
+            ],
+        },
+        {
+            "matrix": [[-2, 5, -1, 4], [8, -6, 3, 1], [2, 2, -4, -1], [-3, 2, 6, -1]],
+        },
+        {
+            "matrix": [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]],
+        },
+        {
+            "matrix": [[-1, -2, -3], [-4, -5, -6], [-7, -8, -9], [-10, -11, -12]],
+        },
+        {
+            "matrix": [[1, -2, 3], [4, -5, 6], [7, -8, 9]],
+        },
+        {
+            "matrix": [[1, 2], [3, 4]],
+        },
+        {
+            "matrix": [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]],
+        },
+        {
+            "matrix": [[-1, -2, -3], [-4, -5, -6], [-7, -8, -9], [-10, -11, -12]],
+        },
+        {
+            "matrix": [[1, -2, 3], [4, -5, 6], [7, -8, 9]],
+        },
+        {
+            "matrix": [[1, 2], [3, 4]],
+        },
+        {
+            "matrix": [
+                [2, -1, 4, -6, 2],
+                [-3, 2, -1, 4, -3],
+                [1, -5, 2, -1, 5],
+                [4, -2, 3, 7, -2],
+            ],
+        },
+        {
+            "matrix": [[-2, 5, -1, 4], [8, -6, 3, 1], [2, 2, -4, -1], [-3, 2, 6, -1]],
+        },
+        {
+            "matrix": [[1, 1], [1, 1], [1, 1], [1, 1]],
+        },
+    ]
 
-    # Genetic Programming
-    start_time = time.time()
-    i1_genetic, i2_genetic, j1_genetic, j2_genetic = max_segment_2d_genetic(matrix)
-    max_sum_genetic = matrix[i1_genetic:i2_genetic + 1, j1_genetic:j2_genetic + 1].sum()
-    end_time = time.time()
-    print("Genetic Programming:")
-    print(f"Maximum Segment Sum: {max_sum_genetic}")
-    print(f"Indices (i1, i2, j1, j2): ({i1_genetic}, {i2_genetic}, {j1_genetic}, {j2_genetic}")
-    print(f"Execution Time: {end_time - start_time} seconds")
-    print("="*30)
+    # Iterate through the test cases
+    for test in tests:
+        matrix = np.array(test["matrix"])
 
-    # Plotting comparison
-    algorithm_names = ["Genetic Programming"]
-    max_sums = [max_sum_genetic]
-    algorithm_name = "Genetic Programming"
-    plot_algorithm_comparison(algorithm_names, max_sums, algorithm_name)
+        # Apply the genetic algorithm
+        start_time = time.time()
+        i1_genetic, i2_genetic, j1_genetic, j2_genetic = max_segment_2d_genetic(matrix)
+        end_time = time.time()
 
-# call fonction main
+        max_sum_genetic = matrix[i1_genetic:i2_genetic + 1, j1_genetic:j2_genetic + 1].sum()
+        execution_time_genetic = end_time - start_time
+
+        # Print the results in a similar format to the provided example
+        print("Genetic Programming:")
+        print(f"Matrix:")
+        print(matrix)
+        print(f"Maximum Segment Sum: {max_sum_genetic}")
+        print(f"Indices (i1, i2, j1, j2): ({i1_genetic}, {i2_genetic}, {j1_genetic}, {j2_genetic}")
+        print(f"Execution Time: {execution_time_genetic} seconds")
+        print("=" * 30)
+
+        # Plot the matrix
+        plt.xlabel('Algorithms')
+        plt.ylabel('Maximum Segment Sum')
+        plt.imshow(matrix, cmap='viridis')
+        plt.title(f'Original Matrix\nMaximum Segment Sum: {max_sum_genetic}')
+        plt.colorbar()
+        plt.show()
+
+        # Plot the selected submatrix
+        plt.imshow(matrix[i1_genetic:i2_genetic + 1, j1_genetic:j2_genetic + 1], cmap='viridis')
+        plt.title('Selected Submatrix')
+        plt.colorbar()
+        # Annotate the plot with Maximum Segment Sum
+        plt.annotate(f'Max Sum: {max_sum_genetic}', xy=(0.5, 0.5), xytext=(30, 10),
+                     textcoords='offset points', color='white', fontsize=12, bbox=dict(facecolor='black', alpha=0.7))
+        plt.show()
+
+# Call the main function
 if __name__ == "__main__":
     main()
+
+
