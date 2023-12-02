be66105353913a0809d4c15a6bebef2b2663b0d8
Amgad-Hesham <amgadkhalil200006@gmail.com>
2023-12-02 11:37:27 +0100

    updated test class
    -test class returns now a dictionary with all the results

--- test_class/test_algorithm.py
+++ test_class/test_algorithm.py
@@ -4,95 +4,53 @@ from test_samples.test_non_constraint import non_constrained_tests
 from test_samples.test_constraint_variable_K_L import constrained_tests_variable_K_L 
 from test_samples.test_constraint_fixed_K_L import constrained_tests_fixed_K_L 
 
-from brute.helpers import print_submatrix, calculate_subarray_sum
+from brute.helpers import print_submatrix,calculate_subarray_sum
 
 class TestAlgorithm(unittest.TestCase):
     """HELPER FUNCTIONS"""
-
-    def is_valid_matrix(self, matrix):
-        """
-        Check if a matrix is valid based on certain criteria.
-
-        Parameters:
-        - matrix: The input matrix to be checked.
-
-        Returns:
-        - True if the matrix is valid, False otherwise.
-        - A string describing the reason for invalidity if applicable.
-        """
-
-        # Check if the matrix is empty
+    # check if its non rectangular
+    def is_non_rectangular(self,matrix):
+        return any(len(row) != len(matrix[0]) for row in matrix)
+    
+    # check if its a one dimensional matrix
+    def is_one_dimensional(self, matrix):
+        return isinstance(matrix, list) and all(isinstance(row, (int, float)) for row in matrix)
+    
+    # Check if the matrix is empty
+    def is_empty(self, matrix):
         if not matrix:
-            return False, "Matrix is empty."
+            return True
+    # Check if the matrix is all numerical values
+    def has_non_numeric_values(self,matrix):
+        return any(not isinstance(element, (int, float)) for row in matrix for element in row)
+    
 
-        # Check if the matrix is one-dimensional
-        if isinstance(matrix, list) and any(isinstance(row, (int, float)) for row in matrix):
-            return False, "Matrix is one-dimensional."
-
-        # Check if the matrix is non-rectangular
-        if any(len(row) != len(matrix[0]) for row in matrix):
-            return False, "Matrix is non-rectangular."
-
-        # Check if the matrix contains non-numeric values
-        if any(not isinstance(element, (int, float)) for row in matrix for element in row):
-            return False, "Matrix contains non-numeric values."
-
-        # If all checks pass, the matrix is valid
-        return True, "Matrix is valid."
-
-    def print_success_information(self, i, result, expected_result, top_left, bottom_right, expected_indices, matrix):
-        """
-        Print information about a successful test case.
-
-        Parameters:
-        - i: Test case number.
-        - result: Result from the algorithm.
-        - expected_result: Expected result for the test case.
-        - top_left: Top-left indices of the submatrix.
-        - bottom_right: Bottom-right indices of the submatrix.
-        - expected_indices: Expected indices of the submatrix.
-        - matrix: The input matrix.
-        """
+    def print_success_inofrmation(self,i,result,expected_result,top_left,bottom_right,expected_indices,matrix):
         print(f"\nTest Case {i + 1} \033[92msucceeded\033[0m")
         print(f"Result: {result}")
         print(f"Expected Result: {expected_result}")
         print(f"Indices: {top_left} to {bottom_right}")
         print(f"Expected Indices: {expected_indices[0]} to {expected_indices[1]}")
         print(f"Matrix:")
-        # Print the matrix used
+        # print the matrix used
         for row in matrix:
             print(row)
-        # Print the submatrix answer
+        #  print the submatrix answer
         print_submatrix(matrix, top_left[0], top_left[1], bottom_right[0], bottom_right[1])
+
 
     """RUNNING AN ALGORITHM AGAINST ALL CASES
     
-    The class includes a method named run_algorithm_tests(algorithm, algorithm_name, constrained_type=None),
-    which executes the algorithm against a set of predefined test cases. The method takes three parameters:
-
-        - algorithm: The algorithm to be tested.
-        - algorithm_name: A string representing the name of the algorithm.
-        - constrained_type: A string indicating the type of constraints for the test cases 
-        (either "variable," "fixed," or None for unconstrained tests).
+    this is the test function that takes two inputs (algorithm_funciton, algorithm_name)
+    - it prints all the test cases, the success rate
+    - it returns a size_time list which is a list of tuples containing (running time for each run, elements in the matrix)
+    - it returns number of successful runs
+    - it returns number of failed runs
     
-    Test Results:
-       - The method executes the algorithm against each test case, providing detailed information for each test,
-       - including success, failure, or skipped status. It prints information such as the test number, result,
-       - expected result, indices, matrix, and submatrix.
-
-    The method returns a dictionary named test_results, which includes the following key-value pairs:
-
-        - size_run_time_test: A list of tuples containing the execution time and the number of elements for each test case.
-        - successes: The number of successful test cases.
-        - failures: The number of failed test cases.
-        - skipped: The number of skipped test cases due to invalid input matrices.
-        - success_percentage: The percentage of successful test cases out of the total valid test cases.
-        - constraints: A list of tuples containing the constraints for each test case (k,l) or empty list if no constraints
     """  
     
                     
-    def run_algorithm_tests(self, algorithm, algorithm_name, constrained_type=None):
-        # Check which type of constraint is used for testing 
+    def run_algorithm_tests(self, algorithm, algorithm_name,constrained_type=None):
         if constrained_type == "variable":
             tests = constrained_tests_variable_K_L
             test_type = "WITH VARIABLE CONSTRAINTS"
@@ -104,26 +62,41 @@ class TestAlgorithm(unittest.TestCase):
             test_type = "WITHOUT CONSTRAINTS"
 
         print(f"\nTESTING {algorithm_name} {test_type}\n\n================================")
-        
-        # Declare variables needed for tests
         successful_tests = 0
         total_tests = len(tests)
-        skipped_tests = 0  # Counts invalid tests
-        failed_tests = 0 
-        size_run_time_test = []  # List of tuples containing (test_time, array_size)
-        constraints = [] # List of tuples containing (k, l)
-        # Loop over all tests
-        for test_number, test_sample in enumerate(tests):
+        skipped_tests = 0 # counts invalid tests
+        failed_tests = 0 # counts failed tests
+        size_run_time_test = []
+        for i, test_sample in enumerate(tests):
             matrix = test_sample["matrix"]
             
-            # Check if matrix is valid before running tests (validation)
-            is_valid, reason = self.is_valid_matrix(matrix)
-            if not is_valid:
-                print(f"\nTest Case {test_number + 1} skipped: {reason}")
+            if not isinstance(matrix,list):
+                print(f"\nTest Case {i + 1} skipped: Input not a matrix.")
+                skipped_tests += 1
+                continue
+            # if empty matrix
+            if self.is_empty(matrix):
+                print(f"\nTest Case {i + 1} skipped: Matrix is empty.")
                 skipped_tests += 1
                 continue
             
-            # Get the expected results from the test and some information about the matrix
+            # if one dimensional matrix
+            if self.is_one_dimensional(matrix):
+                print(f"\nTest Case {i + 1} skipped: Matrix is one-dimensional.")
+                skipped_tests += 1
+                continue
+            
+            # if non rectangular matrix
+            if self.is_non_rectangular(matrix):
+                print(f"\nTest Case {i + 1} skipped: Matrix is non-rectangular.")
+                skipped_tests += 1
+                continue
+            # if it has non numeric values
+            if self.has_non_numeric_values(matrix):
+                print(f"\nTest Case {i + 1} skipped: Matrix contains non-numeric values.")
+                skipped_tests += 1
+                continue
+            
             rows = len(matrix)
             columns = len(matrix[0])
             n_elements = rows * columns
@@ -131,55 +104,46 @@ class TestAlgorithm(unittest.TestCase):
             expected_indices = test_sample["subarray_indices"]
           
             
-            # Start testing 
-            with self.subTest(test_number=test_number + 1):
+
+            with self.subTest(test_number=i + 1):
                 try:
-                    # Calculate the time for the algorithm
+                    # calculate the time for the algorithm
                     start_time = timeit.default_timer()  # Record the start time
-                    
-                    # Check the constraint type and if it is constrained, validate that k and l are less than the size of the rows and columns
-                    if constrained_type == "variable" or constrained_type == "fixed":
+                    if constrained_type == "variable" or constrained_type == "fixed" :
                         k, l = test_sample["constraints"]
-                        
                         if k > rows or l > columns:
-                            print(f"\nTest Case {test_number + 1} skipped: Constraints k or l exceed matrix dimensions.")
+                            print(f"\nTest Case {i + 1} skipped: Constraints k or l exceed matrix dimensions.")
                             skipped_tests += 1
                             continue
-                        
                         result, top_left, bottom_right = algorithm(matrix, k, l)
                     else:
                         result, top_left, bottom_right = algorithm(matrix)
                     end_time = timeit.default_timer()  # Record the end time
                     
-                    # Asserting test results against expected results
                     self.assertEqual(result, expected_result)
+                    
                     self.assertTupleEqual(top_left, expected_indices[0])
                     self.assertTupleEqual(bottom_right, expected_indices[1])
 
-                    # Printing information about test success
-                    self.print_success_information(test_number, result, expected_result, top_left, bottom_right, expected_indices, matrix)
+                    self.print_success_inofrmation(i,result,expected_result,top_left,bottom_right,expected_indices,matrix)
                     successful_tests += 1
                     execution_time = end_time - start_time
-                    size_run_time_test.append((execution_time, n_elements))
-                    if constrained_type == "variable" or constrained_type == "fixed":
-                        constraints.append((k,l))
+                    size_run_time_test.append((execution_time,n_elements))
                     # Print the time taken for the current test
                     print(f"Time taken: {execution_time:.6f} seconds\n")
 
                 except AssertionError as e:
-                    ## Check if the error was raised because it found a different solution, not because it failed
+                    ## check if the error was raised because It found a differnet solution not because it failed
                     if result == expected_result:
                         print("\033[36mAlgorithm Found a different solution than the expected solution but it is the right solution\033[0m")
-                        self.print_success_information(test_number, result, expected_result, top_left, bottom_right, expected_indices, matrix)
+                        self.print_success_inofrmation(i,result,expected_result,top_left,bottom_right,expected_indices,matrix)
                         execution_time = end_time - start_time
-                        size_run_time_test.append((execution_time, n_elements))
+                        size_run_time_test.append((execution_time,n_elements))
                         successful_tests += 1
                         print(f"Time taken: {execution_time:.6f} seconds\n")
-                        if constrained_type == "variable" or constrained_type == "fixed":
-                            constraints.append((k,l))
                         continue
 
-                    print(f"\nTest Case {test_number + 1} \033[91mfailed\033[0m: {e}")
+                    print(f"\nTest Case {i + 1} \033[91mfailed\033[0m: {e}")
                     print(f"Result: {result}")
                     print(f"Expected Result: {expected_result}")
                     print(f"Indices: {top_left} to {bottom_right}")
@@ -187,24 +151,19 @@ class TestAlgorithm(unittest.TestCase):
                     print(f"Matrix:")
                     for row in matrix:
                         print(row)
-                    # Print the submatrix answer
+                    # # print the submatrix answer
                     print(f"Submatrix:")
                     print_submatrix(matrix, top_left[0], top_left[1], bottom_right[0], bottom_right[1])
-                    # Increment the counter if the test fails
-                    failed_tests += 1
+                    # increment the counter if the test fails
+                    failed_tests+=1
                     continue
 
-        success_percentage = (successful_tests / (total_tests - skipped_tests)) * 100
+        success_percentage = (successful_tests / (total_tests-skipped_tests)) * 100
         print(f"\033[92mSuccess Percentage for {algorithm_name}: {success_percentage:.2f}%\033[0m")
         print(f"\033[92m{successful_tests} Successes\033[0m, \033[91m{failed_tests} Fails\033[0m, \033[93m{skipped_tests} Skipped invalid tests\033[0m")
-        test_results = {'size_run_time_test': size_run_time_test,
-                        'successes': successful_tests, 
-                        'failures': failed_tests,
-                        'skipped': skipped_tests,
-                        'success_percentage': success_percentage,
-                        'constraints': constraints,
-                        'constraints_type': constrained_type if constraints else None}
-        return test_results
+        # calculate the average time 
+        
+        return size_run_time_test,successful_tests,failed_tests
 
 if __name__ == '__main__':
     unittest.main()
--- visualizations/visualize.py
+++ visualizations/visualize.py
@@ -1,66 +1,29 @@
-
-
 import matplotlib.pyplot as plt
-import numpy as np
+
+def plot_time_vs_size(data,algorithm_name):
+    """
+    Plot a line graph where the x-axis represents the size of the matrix (rows x columns) and
+    the y-axis represents the time taken for the test case.
+
+    :param data: A sorted array of tuples [(time_for_test_case, size_of_matrix), ...].
+    :param algorithm_name: The name of the algorithm
+    """
+    # Unpacking the data into two separate lists
+
+    times, sizes = zip(*data)
 
 
-def create_test_result_plots(test_results, algorithm_name):
-    constraints = test_results['constraints']
-    constraints_type = test_results['constraints_type']
-    size_run_time_test = test_results['size_run_time_test']
-    
-    
+    # Creating the plot
+    plt.figure(figsize=(10, 6))
+    plt.plot(sizes, times, marker='o')
 
-    # Bar Chart for Successes, Failures, and Skipped
-    labels = ['Successes', 'Failures', 'Skipped']
-    counts = [test_results['successes'], test_results['failures'], test_results['skipped']]
-    plt.figure(figsize=(8, 5))
-    plt.bar(labels, counts, color=['green', 'red', 'yellow'])
-    plt.title(f'Test Results for {algorithm_name}')
-    plt.xlabel('Test Result')
-    plt.ylabel('Number of Test Cases')
+    # Setting the labels and title
+    plt.xlabel('Size of Matrix (rows x columns)')
+    plt.ylabel('Time for Test Case (s)')
+    plt.title(f'Time vs Size of Matrix using {algorithm_name} ')
+
+    # Displaying the plot
     plt.show()
-
-    # Percentage Pie Chart
-    labels = ['Successes', 'Failures']
-    sizes = [test_results['successes'], test_results['failures']]
-    plt.figure(figsize=(6, 6))
-    plt.pie(sizes, labels=labels, autopct='%1.1f%%', colors=['green', 'red'], startangle=90)
-    plt.title(f'Test Results Percentage for {algorithm_name}')
-    plt.show()
-
-    # line plot for Size vs. Run Time
-
-
-    # if constraints and constraints_type == 'variable':
-    #     msg = f"With Variable constraints and fixed array size of {size_run_time_test[0][1]} "
-    #     k_values, l_values = zip(*constraints) 
-    #     constraints_size = [k * l for k,l in zip(k_values,l_values)]
-    #     run_times = [run_time for run_time, _ in size_run_time_test]
-    #     plt.figure(figsize=(8, 5))
-    #     plt.plot(constraints_size, run_times, marker = 'o',color='blue', alpha=0.5)
-    #     plt.title(f'Size vs. Run Time for {algorithm_name} {msg}')
-    #     plt.xlabel('Constraints Size')
-    #     plt.ylabel('Run Time (seconds)')
-    #     plt.show()
-    # else:
-    #     msg = f"With fixed constraints (2,3)" if constraints_type == 'fixed' else ""
-    #     sizes = [size for _, size in size_run_time_test]
-    #     run_times = [run_time for run_time, _ in size_run_time_test]
-    #     plt.figure(figsize=(8, 5))
-    #     plt.plot(sizes, run_times, marker = 'o',color='blue', alpha=0.5)
-    #     plt.title(f'Size vs. Run Time for {algorithm_name} {msg}')
-    #     plt.xlabel('Matrix Size')
-    #     plt.ylabel('Run Time (seconds)')
-    #     plt.show()
-    
-
-
-
-
-    # ... Add more plots as needed ...
-
-
 
 
 
