6d2474bdb63fa38b7bf87bd06fadca2d7e28f8fa
Amgad-Hesham <amgadkhalil200006@gmail.com>
2023-12-02 16:55:05 +0100

    removed greedy constraind

--- visualizations/visualize.py
+++ visualizations/visualize.py
@@ -12,11 +12,17 @@ def create_test_result_plots(test_results, algorithm_name):
     
 
     # Bar Chart for Successes, Failures, and Skipped
+    msg = ""
+    if constraints and constraints_type == 'variable':
+        msg = f"With Variable constraints and fixed array size of {size_run_time_test[0][1]}"
+    else:
+        msg = f"With fixed constraints ({constraints[0][0]},{constraints[0][1]})" if constraints_type == 'fixed' else ""
+        
     labels = ['Successes', 'Failures', 'Skipped']
     counts = [test_results['successes'], test_results['failures'], test_results['skipped']]
     plt.figure(figsize=(8, 5))
     plt.bar(labels, counts, color=['green', 'red', 'yellow'])
-    plt.title(f'Test Results for {algorithm_name}')
+    plt.title(f'Test Results for {algorithm_name} {msg}')
     plt.xlabel('Test Result')
     plt.ylabel('Number of Test Cases')
     plt.show()
@@ -26,33 +32,33 @@ def create_test_result_plots(test_results, algorithm_name):
     sizes = [test_results['successes'], test_results['failures']]
     plt.figure(figsize=(6, 6))
     plt.pie(sizes, labels=labels, autopct='%1.1f%%', colors=['green', 'red'], startangle=90)
-    plt.title(f'Test Results Percentage for {algorithm_name}')
+    plt.title(f'Test Results Percentage for {algorithm_name} {msg}')
     plt.show()
 
     # line plot for Size vs. Run Time
 
 
-    # if constraints and constraints_type == 'variable':
-    #     msg = f"With Variable constraints and fixed array size of {size_run_time_test[0][1]} "
-    #     k_values, l_values = zip(*constraints) 
-    #     constraints_size = [k * l for k,l in zip(k_values,l_values)]
-    #     run_times = [run_time for run_time, _ in size_run_time_test]
-    #     plt.figure(figsize=(8, 5))
-    #     plt.plot(constraints_size, run_times, marker = 'o',color='blue', alpha=0.5)
-    #     plt.title(f'Size vs. Run Time for {algorithm_name} {msg}')
-    #     plt.xlabel('Constraints Size')
-    #     plt.ylabel('Run Time (seconds)')
-    #     plt.show()
-    # else:
-    #     msg = f"With fixed constraints (2,3)" if constraints_type == 'fixed' else ""
-    #     sizes = [size for _, size in size_run_time_test]
-    #     run_times = [run_time for run_time, _ in size_run_time_test]
-    #     plt.figure(figsize=(8, 5))
-    #     plt.plot(sizes, run_times, marker = 'o',color='blue', alpha=0.5)
-    #     plt.title(f'Size vs. Run Time for {algorithm_name} {msg}')
-    #     plt.xlabel('Matrix Size')
-    #     plt.ylabel('Run Time (seconds)')
-    #     plt.show()
+    if constraints and constraints_type == 'variable':
+        msg = f"With Variable constraints and fixed array size of {size_run_time_test[0][1]} "
+        k_values, l_values = zip(*constraints) 
+        constraints_size = [k * l for k,l in zip(k_values,l_values)]
+        run_times = [run_time for run_time, _ in size_run_time_test]
+        plt.figure(figsize=(8, 5))
+        plt.plot(constraints_size, run_times, marker = 'o',color='blue', alpha=0.5)
+        plt.title(f'Size vs. Run Time for {algorithm_name} {msg}')
+        plt.xlabel('Constraints Size ( k * l )')
+        plt.ylabel('Run Time (seconds)')
+        plt.show()
+    else:
+        msg = f"With fixed constraints ({constraints[0][0]},{constraints[0][1]})" if constraints_type == 'fixed' else ""
+        sizes = [size for _, size in size_run_time_test]
+        run_times = [run_time for run_time, _ in size_run_time_test]
+        plt.figure(figsize=(8, 5))
+        plt.plot(sizes, run_times, marker = 'o',color='blue', alpha=0.5)
+        plt.title(f'Size vs. Run Time for {algorithm_name} {msg}')
+        plt.xlabel('Matrix Size')
+        plt.ylabel('Run Time (seconds)')
+        plt.show()
     
 
 
